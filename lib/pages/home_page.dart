734308280a2e423c1a285b2d1a7cf10432c340dd
MerinBabu <merinkachira@gmail.com>
2020-07-30 00:46:09 +0530

    set header

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_web/styles/style.dart';
 
 class Home extends StatefulWidget {
   @override
@@ -12,9 +13,72 @@ class _HomeState extends State<Home> {
       body: ListView(children: <Widget>[
         Container(
           //margin: EdgeInsets.all(10.0),
-          color: Colors.green,
+          decoration: BoxDecoration(
+            color: Colors.white,
+            boxShadow: [
+              BoxShadow(
+                color: Colors.black,
+                blurRadius: 5.0,
+              )
+            ],
+          ),
+          //color: Colors.white,
           height: 70.0,
           width: MediaQuery.of(context).size.width,
+          child: Row(
+            //mainAxisAlignment: MainAxisAlignment.end,
+            children: <Widget>[
+              Container(
+                color: Colors.white,
+                margin: EdgeInsets.only(left: 200.0),
+                child: FlatButton(
+                  child: Row(children: <Widget>[
+                    Text(
+                      'FARMFAB',
+                      style: TextStyle(
+                        fontFamily: 'Lobster',
+                        fontSize: 30.0,
+                        color: Colors.green,
+                      ),
+                    ),
+                    Icon(
+                      Icons.local_florist,
+                      size: 30.0,
+                      color: Colors.green,
+                    ),
+                  ]),
+                  onPressed: null,
+                ),
+              ),
+              Container(
+                margin: EdgeInsets.only(left: 300.0),
+                child: Row(
+                  children: <Widget>[
+                    FlatButton(
+                      child: Text('HOME', style: buildStyle()),
+                      onPressed: null,
+                    ),
+                    FlatButton(
+                      child: Text('ABOUT', style: buildStyle()),
+                      onPressed: null,
+                    ),
+                    FlatButton(
+                      child: Text('PRODUCTS', style: buildStyle()),
+                      onPressed: null,
+                    ),
+                    FlatButton(
+                      child: Text('TEAM', style: buildStyle()),
+                      onPressed: null,
+                    ),
+                    FlatButton(
+                      child: Text('CONTACT', style: buildStyle()),
+                      onPressed: null,
+                    )
+                  ],
+                ),
+              ),
+            ],
+          ),
         ),
       ]),
     );
---
+++ lib/styles/style.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/cupertino.dart';
+
+import 'package:flutter/material.dart';
+
+TextStyle buildStyle() {
+  return TextStyle(
+    color: Colors.green,
+    fontWeight: FontWeight.normal,
+    fontFamily: 'Oswald',
+    fontSize: 15.0,
+    letterSpacing: 2.0,
+  );
+}
